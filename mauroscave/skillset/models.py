775125e53e229e5037cdd143b4fd464f54fcca06
mauricio <mauriciodtdt@hotmail.com>
2021-07-04 22:18:26 +1000

    date field added to Skill object

--- mauroscave/skillset/models.py
+++ mauroscave/skillset/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Skill(models.Model):
     name = models.CharField(max_length=50)
+    date = models.DateField(null=True, blank=True)
     file = models.FileField(upload_to='files/')
     summary = models.CharField(max_length=300)
 
