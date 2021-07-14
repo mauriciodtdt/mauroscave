7b5dc3b7a2ec41ca986087be0c2e0e2ea4a4ff0c
mauricio <mauriciodtdt@hotmail.com>
2021-07-14 22:44:35 +1000

    Fixed ALLOWED_HOST

--- mauroscave/mauroscave/settings.py
+++ mauroscave/mauroscave/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [167.86.113.187]
+ALLOWED_HOSTS = ['167.86.113.187']
 
 
 # Application definition
