586047425b21acc130c614226418330710c11bbc
mauricio <mauriciodtdt@hotmail.com>
2021-07-18 21:52:56 +1000

    Added UnitTests to homepage

--- mauroscave/thecave/tests.py
+++ mauroscave/thecave/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Safari()
+
+    def test_there_is_homepage(self):
+        self.browser.get('http://127.0.0.1:8000')
+        self.assertIn('Mauricio Diaz', self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+class UnitTestCase(TestCase):
+
+    def test_home_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'thecave/home.html')
